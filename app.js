1a36a488ec9cc9f525a3cb2ccb408c56cc01e68e
Tala Jallad <talaj2911@gmail.com>
2021-01-21 03:54:12 +0200

    edit slider and some css

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function nextSlide() {
 }
 /* Decrease the index by 1 - show the previous slide: */
 function previousSlide() {
-    showSlides(slideIndex -= 1);  
+    showSlides(slideIndex -= 1);
 }
 /* Set the current slide: */
 function currentSlide(n) {
@@ -19,66 +19,70 @@ function showSlides(n) {
     let i;
     /* We refer to the elements with the class name "item", that is, to the pictures: */
     let slides = document.getElementsByClassName("item");
-    
+
     /* Checking the number of slides: */
     if (n > slides.length) {
-      slideIndex = 1
+        slideIndex = 1
     }
     if (n < 1) {
         slideIndex = slides.length
     }
-  
+
     /* Loop through each slide in a for loop: */
     for (let slide of slides) {
         slide.style.display = "none";
     }
     /* Making an element block: */
-    slides[slideIndex - 1].style.display = "block";    
+    slides[slideIndex - 1].style.display = "block";
 }
 
 var arrayOfResult = [];
 // retiving items stored in local-storage then accessing each question element.
 
 
-if (localStorage.length > 0 ) {
-    arrayOfResult = JSON.parse(localStorage.getItem('feedbacks'))
-   // console.log(arrayOfResult)
-var pa1 = document.getElementById('qOne');
-var pa2 = document.getElementById('qTwo');
-var pa3 = document.getElementById('qThree');
-var pa4 = document.getElementById('qFour');
-var pa5 = document.getElementById('qFive');
-var pa6 = document.getElementById('qSix');
-// creating an empty array
+if (localStorage.length > 0) {
+    if (localStorage.getItem('feedbacks') !== null) {
+        arrayOfResult = JSON.parse(localStorage.getItem('feedbacks'))
+    }
 
-var randomIndexes=[]
-//this loop increments the index number
 
-for(var i=0;i<arrayOfResult.length;i++){
-    randomIndexes[i]=i;
-}
+    // console.log(arrayOfResult)
+    var pa1 = document.getElementById('qOne');
+    var pa2 = document.getElementById('qTwo');
+    var pa3 = document.getElementById('qThree');
+    var pa4 = document.getElementById('qFour');
+    var pa5 = document.getElementById('qFive');
+    var pa6 = document.getElementById('qSix');
+    // creating an empty array
 
-shuffleArray(randomIndexes)
-// this renders the values of the array which they indicates the  value (answers) of the questions
-if(arrayOfResult.length>=1){
-pa1.textContent = arrayOfResult[randomIndexes[0]].question;
-}
-if(arrayOfResult.length>=2){
-pa2.textContent = arrayOfResult[randomIndexes[1]].question;
-}
+    var randomIndexes = []
+        //this loop increments the index number
 
-if(arrayOfResult.length>=3){
-pa3.textContent = arrayOfResult[randomIndexes[2]].question;
-}
-if(arrayOfResult.length>=4){
-pa4.textContent = arrayOfResult[randomIndexes[3]].question;
-}
-if(arrayOfResult.length>=5){
-pa5.textContent = arrayOfResult[randomIndexes[4]].question;
-}
-if(arrayOfResult.length>=6){
-pa6.textContent = arrayOfResult[randomIndexes[5]].question;
-}
+    for (var i = 0; i < arrayOfResult.length; i++) {
+        randomIndexes[i] = i;
+    }
+
+    shuffleArray(randomIndexes)
+        // this renders the values of the array which they indicates the  value (answers) of the questions
+    if (arrayOfResult.length >= 1) {
+        pa1.textContent = arrayOfResult[randomIndexes[0]].question;
+    }
+    if (arrayOfResult.length >= 2) {
+        pa2.textContent = arrayOfResult[randomIndexes[1]].question;
+    }
+
+    if (arrayOfResult.length >= 3) {
+        pa3.textContent = arrayOfResult[randomIndexes[2]].question;
+    }
+    if (arrayOfResult.length >= 4) {
+        pa4.textContent = arrayOfResult[randomIndexes[3]].question;
+    }
+    if (arrayOfResult.length >= 5) {
+        pa5.textContent = arrayOfResult[randomIndexes[4]].question;
+    }
+    if (arrayOfResult.length >= 6) {
+        pa6.textContent = arrayOfResult[randomIndexes[5]].question;
+    }
 
 
 }
@@ -94,44 +98,46 @@ function shuffleArray(array) {
     }
 }
 
-/*Search Code*/ 
+/*Search Code*/
 
-var formSearch=document.getElementById("searchForm");
+var formSearch = document.getElementById("searchForm");
 
-formSearch.addEventListener("submit",formSearchHandler);
+formSearch.addEventListener("submit", formSearchHandler);
 
 function formSearchHandler(event) {
     event.preventDefault();
-    var searchable=event.target.search.value;
+    var searchable = event.target.search.value;
     console.log(searchable);
     search(searchable);
 }
-function search(searchable){
-    var bookTitles=getBookTitle();//get array of book title located in local storage
+
+function search(searchable) {
+    var bookTitles = getBookTitle(); //get array of book title located in local storage
     console.log(bookTitles);
     for (let index = 0; index < bookTitles.length; index++) {
-        var tiltle=bookTitles[index].toUpperCase();
-        if(searchable.toUpperCase()==tiltle){
+        var tiltle = bookTitles[index].toUpperCase();
+        if (searchable.toUpperCase() == tiltle) {
             clickHandler(index);
             return;
-        } 
+        }
     }
     swal("No Such Result ", searchable);
 }
+
 function clickHandler(params) {
     localStorage.setItem('currentBookIndex', params);
-    window.location.replace("../category/single.html"); 
-}
-function getBookTitle() {
-    var titles=[]
-    if(localStorage.length!=0)
-    if(localStorage.getItem("books")!=null){
-        var temp=JSON.parse(localStorage.getItem("books"));
-        //console.log(temp);
-        for (let index = 0; index < temp.length; index++) {
-            titles.push(temp[index].title);      
-        }//end for
-    }
-    return titles;
+    window.location.replace("../category/single.html");
 }
 
+function getBookTitle() {
+    var titles = []
+    if (localStorage.length != 0)
+        if (localStorage.getItem("books") != null) {
+            var temp = JSON.parse(localStorage.getItem("books"));
+            //console.log(temp);
+            for (let index = 0; index < temp.length; index++) {
+                titles.push(temp[index].title);
+            } //end for
+        }
+    return titles;
+}
